56d85eff3f85c706d1e014285965ba4e750414dd
Willem Röpke <willem.ropke@vub.be>
2018-11-25 23:52:54 +0100

    Added some very basic checks to prevent errors. Cleaned up code and added comments as well.

--- vid2gif.py
+++ vid2gif.py
@@ -1,49 +1,61 @@
 import imageio  # The python library for reading and writing images. See: http://imageio.github.io/.
 import sys
 
-# Get the name for the new gif from the name of the current file.
+# Create the name of the new gif, by concatenating the current name with the gif filetype.
 def getFileName(filePath):
     print("Getting file name from path.")
     splittedPath = filePath.split('/')              # Split the path.
-    completeFile = splittedPath[-1]                 # Get the last element. This is the destination.
-    fileName = completeFile.split('.')[0]           # Split the filename. We get the name at 0 and the type at 1.
-    gifFile = fileName + '.gif'                     # Combine the name with .gif and return this.
-    return gifFile
+    completeFile = splittedPath[-1]                     # Get the last element. This is the destination.
+    splittedFile = completeFile.split('.')              # Split the filename. We get the name at 0 and the type at 1.
+    return splittedFile
 
-def makeGif(reader, writer, begin, end):
-    print("Converting the file to a gif.")
-    for frame in range(begin, end):
-        writer.append_data(reader.get_data(frame))
+# Check if the filetype of the provided file is supported.
+def validFileType(fileType):
+    return fileType == 'mp4'   # Currently only mp4 is supported.
 
-    writer.close()
-    reader.close()
-    print ("Finished converting!")
-
+# Create the name of the new gif, by concatenating the current name with the gif filetype.
+def makeGifName(fileName):
+    return fileName + '.gif'
 
 # Calculate a specific frame for a given time.
 def calcFrame(time, fps):  
     return time * fps
 
-args = sys.argv
+# Actually create the gif.
+def generateGif(reader, writer, begin, end):
+    print("Converting the file to a gif.")
+    for frame in range(begin, end):
+        writer.append_data(reader.get_data(frame))      # Get the correct frame and write this to the gif file.
 
-filePath = args[1]
-begin = int(args[2])
-end = int(args[3])
-gifFile = getFileName(filePath)
+    writer.close()      # Close the writer and reader.
+    reader.close()
+    print ("Finished converting!")
 
-reader = imageio.get_reader(filePath)
-writer = imageio.get_writer(gifFile)
-metaData = reader.get_meta_data()
-fps = metaData['fps']
-begin = int(calcFrame(begin, fps))
-end = int(calcFrame(end, fps))
-makeGif(reader, writer, begin, end)
+# The function that controls the flow of the script. This gets called with the arguments provided by the user.
+def makeGif(args):
+    filePath = args[1]
+    begin = int(args[2])
+    end = int(args[3])
+    splittedFile = getFileName(filePath)
+    fileName = splittedFile[0]
+    fileType = splittedFile[1]
 
+    if validFileType(fileType):
+        gifName = makeGifName(fileName)
+        reader = imageio.get_reader(filePath)
+        writer = imageio.get_writer(gifName)
+        metaData = reader.get_meta_data()
+        fps = metaData['fps']
+        duration = metaData['duration']
+        maxFrame = calcFrame(duration, fps)
+        begin = int(calcFrame(begin, fps))
+        end = int(calcFrame(end, fps)) 
 
+        if begin <= end and end <= maxFrame:
+            generateGif(reader, writer, begin, end)
+        elif begin > end:
+            print("Your begin time was greater than the end. Please try again.")
+        else:
+            print("Your end time exceeded the total duration of the video. Please retry for an earlier end.")
 
-
-
-
-
-
-
+makeGif(sys.argv)
